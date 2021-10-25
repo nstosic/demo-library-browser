75bbbe44e1904531b03658e53273ab33854069c5
nstosic <nemanjastosic@outlook.com>
2021-10-24 21:40:44 -0700

    Added support for dependency injection

---
+++ lib/dependency_injection/dependent_providers.dart
@@ -0,0 +1,17 @@
+import 'package:demo_books/api/api_client.dart';
+import 'package:demo_books/api/api_serializer.dart';
+import 'package:demo_books/repo/books_repository.dart';
+import 'package:demo_books/util/constants.dart';
+import 'package:provider/provider.dart';
+
+final dependentProviders = [
+  ProxyProvider<ApiSerializer, ApiClient>(
+    lazy: true,
+    update: (context, apiSerializer, instance) =>
+        instance ?? ApiClient(Constants.timeoutDuration, apiSerializer),
+  ),
+  ProxyProvider<ApiClient, BooksRepository>(
+    lazy: true,
+    update: (context, apiClient, _) => BooksRepository(apiClient),
+  ),
+];
