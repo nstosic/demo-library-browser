8beec3f614a23a0ae98c25c22313f88fa8d31118
nstosic <nemanjastosic@outlook.com>
2021-10-24 19:53:41 -0700

    Updated repo layer

---
+++ lib/repo/books_repository.dart
@@ -0,0 +1,24 @@
+import 'package:demo_books/api/api_client.dart';
+import 'package:demo_books/model/book/book.dart';
+import 'package:meta/meta.dart';
+
+@visibleForTesting
+abstract class BooksRepositoryUrls {
+  static const books = '/books';
+}
+
+class BooksRepository {
+  BooksRepository(this._apiClient);
+
+  final ApiClient _apiClient;
+
+  Future<List<Book>> fetchBooks() async {
+    final response = _apiClient.getRemoteCollection<Book>(url: BooksRepositoryUrls.books);
+    return response;
+  }
+
+  Future<Book> fetchBook(Book book) async {
+    final response = _apiClient.getRemoteDocument<Book>(url: book.links!.selfUrl!);
+    return response;
+  }
+}
