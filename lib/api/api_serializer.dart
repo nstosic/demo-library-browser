c858a50fb78dda96a9e16dbdd5015af51a3c3ed9
nstosic <nemanjastosic@outlook.com>
2021-10-24 21:40:11 -0700

    Updated serialization logic in the ApiClient

--- lib/api/api_serializer.dart
+++ lib/api/api_serializer.dart
@@ -1,10 +1,23 @@
 import 'package:demo_books/base/base_exception.dart';
 import 'package:demo_books/model/base_model.dart';
+import 'package:demo_books/model/book/book.dart';
+import 'package:demo_books/model/book/book_attributes.dart';
+import 'package:demo_books/model/book/book_links.dart';
+import 'package:demo_books/model/book/book_relationships.dart';
+import 'package:demo_books/model/meta/one_to_many_relationship.dart';
+import 'package:demo_books/model/meta/one_to_one_relationship.dart';
 import 'package:flutter/material.dart';
 
 class ApiSerializer {
   @visibleForTesting
-  final factories = <Type, BaseModel Function(Map<String, dynamic>)>{};
+  final factories = <Type, BaseModel Function(Map<String, dynamic>)>{
+    Book: (json) => Book.fromJson(json),
+    BookAttributes: (json) => BookAttributes.fromJson(json),
+    BookLinks: (json) => BookLinks.fromJson(json),
+    BookRelationships: (json) => BookRelationships.fromJson(json),
+    OneToOneRelationship: (json) => OneToOneRelationship.fromJson(json),
+    OneToManyRelationship: (json) => OneToManyRelationship.fromJson(json),
+  };
 
   T fromJson<T extends BaseModel>(Map<String, dynamic> json) {
     if (factories[T] != null) {
