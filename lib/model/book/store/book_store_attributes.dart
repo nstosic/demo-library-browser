f74bf4ea360841777705d6618e9f61f69da75a12
nstosic <nemanjastosic@outlook.com>
2021-10-25 18:42:26 -0700

    Fixed linter warnings

--- lib/model/book/store/book_store_attributes.dart
+++ lib/model/book/store/book_store_attributes.dart
@@ -1,6 +1,4 @@
 import 'package:demo_books/model/base_model.dart';
-import 'package:demo_books/model/converters/one_to_many_relationship_converter.dart';
-import 'package:demo_books/model/meta/one_to_many_relationship.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'book_store_attributes.freezed.dart';
