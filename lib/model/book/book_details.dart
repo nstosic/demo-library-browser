d3043975643850fc109c975ca45f7e8aea0a6caa
nstosic <nemanjastosic@outlook.com>
2021-10-25 17:25:36 -0700

    Added equals and hashCode to BookDetails

--- lib/model/book/book_details.dart
+++ lib/model/book/book_details.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:demo_books/model/book/author/book_author.dart';
 import 'package:demo_books/model/book/book.dart';
 import 'package:demo_books/model/book/store/book_store.dart';
@@ -6,12 +7,25 @@ import 'package:meta/meta.dart';
 @immutable
 class BookDetails {
   const BookDetails({
-    required this.book,
     required this.author,
+    required this.book,
     required this.stores,
   });
 
-  final Book book;
   final BookAuthor author;
+  final Book book;
   final List<BookStore> stores;
+
+  @override
+  int get hashCode => author.hashCode ^ book.hashCode ^ stores.hashCode;
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is BookDetails &&
+            (identical(other.author, author) || other.author == author) &&
+            (identical(other.book, book) || other.book == book) &&
+            const DeepCollectionEquality().equals(other.stores, stores));
+  }
 }
